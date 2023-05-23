4adc008b03d3101ba555a0279f8103aa96a80ac2
samswain2 <samswain2256@gmail.com>
2023-05-23 22:07:02 +0000

    Up

--- MSiA 431/03_hw/02_question/part2.py
+++ MSiA 431/03_hw/02_question/part2.py
@@ -1,7 +1,7 @@
 from pyspark import SparkContext, SparkConf
 from datetime import datetime
-import numpy as np
-from itertools import combinations
+from pyspark.mllib.stat import Statistics
+from pyspark.mllib.linalg import Vectors
 
 conf = SparkConf().setAppName("Chicago Crime Data Analysis")
 sc = SparkContext(conf=conf)
@@ -13,9 +13,6 @@ crimeData = sc.textFile("hdfs://wolf:9000/user/sms5736/03_hw/Crimes_-_2001_to_pr
 header = crimeData.first()
 crimeData = crimeData.filter(lambda line: line != header)
 
-# Take a small sample of the data
-# crimeData = crimeData.sample(False, 0.001)  # 1% of the data
-
 def parse(line):
     fields = line.split(',')
     date = datetime.strptime(fields[2], '%m/%d/%Y %I:%M:%S %p')
@@ -26,39 +23,35 @@ def parse(line):
 crimeDataParsed = crimeData.map(parse)
 
 # Filter for the last 5 years
-crimeDataLast5Years = crimeDataParsed.filter(lambda x: x[0].year >= datetime.now().year - 5)
+most_recent_date = datetime(2019, 12, 31)  # set the most recent date
+crimeDataLast5Years = crimeDataParsed.filter(lambda x: x[0].year >= most_recent_date.year - 5)
 
-# Count the number of crimes per beat per year
-beatCountsPerYear = crimeDataLast5Years.map(lambda x: ((x[1], x[0].year), 1)).reduceByKey(lambda a, b: a + b)
+# Create a set of all unique beats
+all_beats = crimeDataLast5Years.map(lambda x: x[1]).distinct().collect()
+all_beats_dict = {beat: index for index, beat in enumerate(all_beats)}
+num_beats = len(all_beats)
 
-# Get all beats
-beats = beatCountsPerYear.map(lambda x: x[0][0]).distinct().collect()
+# Count the number of crimes per beat per year and switch the order of beat and year
+beatCountsPerYear = crimeDataLast5Years.map(lambda x: ((x[0].year, x[1]), 1)).reduceByKey(lambda a, b: a + b)
 
-# Sort the counts by year for each beat and persist it
-beatCountsPerYearSorted = beatCountsPerYear.map(lambda x: (x[0][0], (x[0][1], x[1]))).groupByKey().mapValues(sorted).persist()
+# Create a full vector for each year
+beatCountsPerYearFull = beatCountsPerYear.map(lambda x: (x[0][0], (all_beats_dict[x[0][1]], x[1]))).groupByKey()
 
-# Calculate the correlation for each pair of beats
-correlations = []
-for beat1, beat2 in combinations(beats, 2):
-    beat1Counts = beatCountsPerYearSorted.lookup(beat1)[0]
-    beat2Counts = beatCountsPerYearSorted.lookup(beat2)[0]
+def fill_vector(counts):
+    vec = [0] * num_beats
+    for beat_index, count in counts:
+        vec[beat_index] = count
+    return Vectors.dense(vec)
 
-    if len(beat1Counts) != len(beat2Counts):  # if there are missing years for a beat, skip this pair
-        continue
+countsVectors = beatCountsPerYearFull.mapValues(fill_vector)
 
-    beat1CountsValues = [x[1] for x in beat1Counts]
-    beat2CountsValues = [x[1] for x in beat2Counts]
+# Compute correlation matrix
+correlation_matrix = Statistics.corr(countsVectors.values())
 
-    correlation = np.corrcoef(beat1CountsValues, beat2CountsValues)[0, 1]
-    correlations.append(((beat1, beat2), correlation))
-
-# Top x
-num_top = 1000
-
-# Sort by correlation and print the pairs with the highest correlations
-correlations.sort(key=lambda x: x[1], reverse=True)
+# Print Cor matrix
+print(correlation_matrix)
 
 # Open a file to write the result
-with open('top_1000_higest_correlated_beats.txt', 'w') as f:
-    for pair, correlation in correlations[:num_top]:
-        f.write(f'Beats: {pair}, Correlation: {correlation}\n')
+with open('beat_correlation.txt', 'w') as f:
+    for row in correlation_matrix:
+        f.write(f"{row}\n")
