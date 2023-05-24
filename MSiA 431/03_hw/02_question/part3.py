77a337a13e0ed9641f898377c413889b918b72d4
samswain2 <samswain2256@gmail.com>
2023-05-23 23:44:53 -0500

    Worked on part 3

--- MSiA 431/03_hw/02_question/part3.py
+++ MSiA 431/03_hw/02_question/part3.py
@@ -21,43 +21,39 @@ def parse(line):
     return (date, block)
 
 # Parse the RDD
-crimeDataParsed = crimeData.map(parse)
+crimeDataParsed = crimeData.map(lambda line: (line.split(',')[3], 1))
 
 # Filter for the time during which each mayor was in office
-# NOTE: Replace with the actual dates of each mayor's term
-dalyCrimes = crimeDataParsed.filter(lambda x: datetime(1989, 4, 24) <= x[0] <= datetime(2011, 5, 16))
-emanuelCrimes = crimeDataParsed.filter(lambda x: datetime(2011, 5, 16) < x[0] <= datetime(2019, 5, 20))
-
-for date, block in dalyCrimes.collect()[:100]:
-    print(f"Year: {date.year}, Block: {block}")
+dalyCrimes = crimeDataParsed.filter(lambda x: datetime(1989, 4, 24) <= x[1] <= datetime(2011, 5, 16))
+emanuelCrimes = crimeDataParsed.filter(lambda x: datetime(2011, 5, 16) < x[1] <= datetime(2019, 5, 20))
 
 # Calculate sum and count for each block during each mayor's term
 def seqOp(x, y):
-    return (x[0] + 1, x[1] + y, x[2] + y*y)
+    return (x[0] + y, x[1] + y, x[2] + y*y)
 
 def combOp(x, y):
     return (x[0] + y[0], x[1] + y[1], x[2] + y[2])
 
 zeroValue = (0, 0.0, 0.0)
 
-# dalyBlockStats = dalyCrimes.aggregateByKey(zeroValue, seqOp, combOp)
-# emanuelBlockStats = emanuelCrimes.aggregateByKey(zeroValue, seqOp, combOp)
+dalyBlockStats = dalyCrimes.aggregateByKey(zeroValue, seqOp, combOp)
+emanuelBlockStats = emanuelCrimes.aggregateByKey(zeroValue, seqOp, combOp)
 
-# # Calculate mean and standard deviation for each block during each mayor's term
-# def calcStats(sum_count):
-#     count, sum, sumSq = sum_count
-#     mean = sum / count
-#     std_dev = sqrt(sumSq / count - mean*mean)
-#     return (mean, std_dev)
+# Calculate mean and standard deviation for each block during each mayor's term
+def calcStats(sum_count_sqcount):
+    count, sum, sqcount = sum_count_sqcount
+    mean = sum / count
+    std_dev = sqrt(sqcount / count - mean*mean)
+    return (mean, std_dev, count)
 
-# dalyBlockStats = dalyBlockStats.mapValues(calcStats)
-# emanuelBlockStats = emanuelBlockStats.mapValues(calcStats)
+dalyBlockStats = dalyBlockStats.mapValues(calcStats)
+emanuelBlockStats = emanuelBlockStats.mapValues(calcStats)
 
-# # Save the statistics for each block during each mayor's term
-# with open('daly_block_stats.txt', 'w') as f:
-#     for key, value in dalyBlockStats.collect():
-#         f.write(str(key) + ': ' + str(value) + '\n')
+# Save the statistics for each block during each mayor's term
+with open('daly_block_stats.txt', 'w') as f:
+    for key, value in dalyBlockStats.collect():
+        f.write(str(key) + ': ' + str(value) + '\n')
 
-# with open('emanuel_block_stats.txt', 'w') as f:
-#     for key, value in emanuelBlockStats.collect():
-#         f.write(str(key) + ': ' + str(value) + '\n')
+with open('emanuel_block_stats.txt', 'w') as f:
+    for key, value in emanuelBlockStats.collect():
+        f.write(str(key) + ': ' + str(value) + '\n')
